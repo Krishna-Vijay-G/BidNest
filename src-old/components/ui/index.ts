099ff26123836da14ed90a141839daa38e4239f2
Krishna-Vijay-G <krishnavijay.gkv@gmail.com>
2026-02-20 14:16:48 +0530

    removed old codes

--- src-old/components/ui/index.ts
+++
@@ -1,7 +0,0 @@
-export { Card, StatCard } from './Card';
-export { Button } from './Button';
-export { Modal } from './Modal';
-export { Input, TextArea, Select } from './Input';
-export { Badge, StatusBadge } from './Badge';
-export { LoadingSpinner, PageLoader, TableSkeleton } from './Loading';
-export { EmptyState } from './EmptyState';
